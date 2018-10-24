a4097fb31ae044c1e75d178e0876335570d0c90c
muntasir2165 <muntasir2165@hotmail.com>
2018-10-24 18:43:47 -0400

    added the dotenv package for getting the port number from the environment in heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
@@ -9,7 +10,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
